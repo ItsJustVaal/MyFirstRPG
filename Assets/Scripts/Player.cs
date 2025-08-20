11b687472b81aa875123d87a39bf0233ea249e70
Robert <Robmmar90@gmail.com>
2025-08-20 12:14:36 -0700

    created and updated state machines, added player controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,43 @@
+using System.Numerics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerInputSet input;
+    private StateMachine stateMachine;
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
+    public UnityEngine.Vector2 moveInput { get; private set; }
+
+    private void Awake()
     {
-        
+        stateMachine = new StateMachine();
+        input = new PlayerInputSet();
+
+        idleState = new PlayerIdleState(this, stateMachine, "idle");
+        moveState = new PlayerMoveState(this, stateMachine, "move");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        input.Enable();
+
+        input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<UnityEngine.Vector2>();
+        input.Player.Movement.canceled += ctx => moveInput = UnityEngine.Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Start()
+    {
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        stateMachine.UpdateActiveState();
     }
 }
